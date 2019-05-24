5cda3e12bae941f738353d472a27746071be5273
Lucas Ahrens <lucahrens@icloud.com>
2019-05-23 21:03:36 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,16 @@ public class Main {
 
   public static void main(String[] args) {
 	  try {
-		Connection con=DriverManager.getConnection("jdbc:mysql://hackathon.cliosjusorja.us-east-1.rds.amazonaws.com:3306/Hackathon","Hackathon","Hackathon");
+		Connection con=DriverManager.getConnection("jdbc:mysql://hackathon.cliosjusorja.us-east-1.rds.amazonaws.com:3306/Questions","Hackathon","Hackathon");
 		  System.out.println("hey");
+		  	Statement myStmt = con.createStatement();
+		  //you have to change the 69 because the id has to be differnet every time
+		  	myStmt.execute("INSERT INTO questionsTable " + "VALUES ( '69', 'Test', 'holy hsit')");
+		  	
+
 	} catch (SQLException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}  
   }
- 
-}
+ }
