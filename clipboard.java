d46f19d45aac500880bc665f9d630c42afd7228f
Lucas Ahrens <lucahrens@icloud.com>
2019-05-26 08:43:13 -0400

    Delete clipboard.java

--- clipboard.java
+++
@@ -1,4 +0,0 @@
-Toolkit toolkit = Toolkit.getDefaultToolkit();
-				Clipboard clipboard = toolkit.getSystemClipboard();
-				String result = (String) clipboard.getData(DataFlavor.stringFlavor);
-				System.out.println("String from Clipboard:" + result);
